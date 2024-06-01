5f376a567d3ad2599a3bc57068eefa0baced4eba
Phan Nam <147438273+PhanNam1501@users.noreply.github.com>
2024-06-01 12:49:30 +0700

    Update CartTest.java

--- Aimsproject/src/hust/soict/cybersec/test/cart/CartTest.java
+++ Aimsproject/src/hust/soict/cybersec/test/cart/CartTest.java
@@ -1,24 +1,24 @@
 package hust.soict.cybersec.test.cart;
 
 import hust.soict.cybersec.aims.cart.Cart;
-import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
+
+import hust.soict.cybersec.aims.media.DigitalVideoDisc;
 
 public class CartTest {
 	public static void main(String[] args) {
 		Cart cart = new Cart();
 		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 85, 19.95f);
-		cart.addDigitalVideoDisc(dvd1);
+		cart.addMedia(dvd1);
 		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		cart.addDigitalVideoDisc(dvd2);
+		cart.addMedia(dvd2);
 		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		cart.addDigitalVideoDisc(dvd3);
-		
-		System.out.println(cart.print());
-		cart.searchById(4);
-		cart.searchByTitle("The Lion King");
+		cart.addMedia(dvd3);
+		cart.print();
+		cart.searchId(4);
+		cart.searchTitle("The Lion King");
 		
 	}
 }
